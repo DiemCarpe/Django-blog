e825ccb79950b8192f8f6224cd442f9d202eb649
CarpeDiem <yihannan7@163.com>
2020-06-29 21:52:00 +0800

    '21.51'

--- comments/templatetags/comments_extras.py
+++ comments/templatetags/comments_extras.py
@@ -4,7 +4,7 @@ from ..forms import  CommentForm
 
 register=template.Library()
 
-@register.inclusion_tag('comments/inclusions/_form.html')
+@register.inclusion_tag('comments/inclusions/_form.html',takes_context=True)
 
 def show_comment_form(context,post,form=None):
     if form is None:
